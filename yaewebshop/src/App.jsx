97add8aa419983fe6b114852b7b7232c44077fff
Yuki Emit <167169899+Yukiemit@users.noreply.github.com>
2026-02-05 01:31:20 +0800

    Delete yaewebshop directory

--- yaewebshop/src/App.jsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ padding: 40, fontSize: 30 }}>
-      IKUY TEST ✅ (If you see this, React is running the right App.jsx)
-    </div>
-  );
-}
--- yaewebshop/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  // Makes built asset paths work inside /[initials]webshop/ folder
-  base: "./",
-});
